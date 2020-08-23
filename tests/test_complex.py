586ae7098b9e2262e3184032cfc1a5f8cebb24b1
Jason Lin <jason40418@gmail.com>
2020-08-23 21:18:55 +0800

    [Add] new test case

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -5,6 +5,9 @@ from python_travis.arithmetic import Arithmetic
 def test_arith_add():
     assert(2 * 10 + Arithmetic(10, 20).add() == 50)
 
+def test_arith_add2():
+    assert(2 * 10 + Arithmetic(10, 20).add() == 50)
+
 def test_arith_minus():
     assert(Arithmetic(10, 20).minus() == -10)
 
