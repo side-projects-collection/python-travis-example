3d58f2e45d43842fc71384512717892b9a3fb416
Jason Lin <jason40418@gmail.com>
2020-08-23 19:38:46 +0800

    [Fix] Unit-test method use error

--- tests/test_arith.py
+++ tests/test_arith.py
@@ -9,4 +9,4 @@ def test_arith_minus():
     assert(Arithmetic(10, 20).minus() == -10)
 
 def test_arith_multiple():
-    assert(Arithmetic(10, 20).minus() == 200)
+    assert(Arithmetic(10, 20).multiple() == 200)
